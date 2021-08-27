31b9bc4129940b10e854130af22ba082f298b877
Jaakko Heusala <jheusala@iki.fi>
2021-08-27 04:03:19 +0300

    Added pathsToScalarItems()

--- modules/lodash.ts
+++ modules/lodash.ts
@@ -532,6 +532,72 @@ export function parseNonEmptyString (value: any) : string | undefined {
     return `${value}`;
 }
 
+/**
+ * Returns path to every scalar item in the variable.
+ *
+ * @param value
+ * @param baseKey
+ * @returns Every path to scalar properties.
+ *          If the value is not an array or object, will return the baseKey itself if it's defined.
+ *          If the baseKey is not defined or is empty, will return an empty array.
+ */
+export function pathsToScalarItems (
+    value   : any,
+    baseKey : string = ''
+) : string[] {
+
+    if (isArray(value)) {
+
+        let allKeys : string[] = [];
+
+        forEach(
+            value,
+            (item : any, itemIndex: number) => {
+
+                const itemKey = `${baseKey}${baseKey?'.':''}${itemIndex}`;
+
+                const allItemKeys = pathsToScalarItems(item, itemKey);
+
+                allKeys = allKeys.concat(allItemKeys);
+
+            }
+        );
+
+        return allKeys;
+
+    }
+
+    if (isObject(value)) {
+
+        let allKeys : string[] = [];
+
+        forEach(
+            keys(value),
+            (itemKey : any, itemIndex: number) => {
+
+                const itemFullKey = `${baseKey}${baseKey?'.':''}${itemKey}`;
+
+                const item : any = value[itemKey];
+
+                const allItemKeys = pathsToScalarItems(item, itemFullKey);
+
+                allKeys = allKeys.concat(allItemKeys);
+
+            }
+        );
+
+        return allKeys;
+
+    }
+
+    if (baseKey === '') {
+        return [];
+    }
+
+    return [baseKey];
+
+}
+
 export {
     map,
     get,
