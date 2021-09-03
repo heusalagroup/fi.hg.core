2a10c6694f282f5b85826ed49d27fe95eb6a33ad
Jaakko Heusala <jheusala@iki.fi>
2021-09-03 11:56:34 +0300

    Fixed type errors

--- StringUtils.ts
+++ StringUtils.ts
@@ -75,7 +75,7 @@ export class StringUtils {
                     variablePrefix,
                     variableSuffix,
                     defaultValue
-                )
+                ) as ReadonlyJsonAny
             );
         }
 
