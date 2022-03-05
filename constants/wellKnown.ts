cb68ac671bc1a5c27a4975d757fa03a5d4f54f6e
Jaakko Heusala <jhh@heusalagroup.fi>
2022-03-05 18:17:58 +0200

    Fixed well known contants

--- constants/wellKnown.ts
+++ constants/wellKnown.ts
@@ -1,4 +1,4 @@
-
+// Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
 export const WELL_KNOWN_HG_METADATA_SERVICE_END_POINT = '/.well-known/fi.hg.m.json'
 export const HG_METADATA_SERVICE_GITHUB_ORGANIZATION_NAME = 'fi.hg.github.org';
