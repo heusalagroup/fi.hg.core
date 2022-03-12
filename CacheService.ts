2e1b82c54e5b9e7d57b4dd0189a85605e4a8a973
Jaakko Heusala <jhh@heusalagroup.fi>
2022-03-12 19:56:09 +0200

    Added CacheService

---
+++ CacheService.ts
@@ -0,0 +1,38 @@
+// Copyright (c) 2021-2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+
+import { reduce } from "./modules/lodash";
+
+export interface CacheClearCallback {
+    () : Promise<void> | void;
+}
+
+/**
+ * This service is used to clear any internal caches.
+ *
+ * It is implemented mainly for the ReactJS SSR server so that requests can clear internal state between requests.
+ */
+export class CacheService {
+
+    private static _clearCallbacks : CacheClearCallback[] = [];
+
+    public static registerClearCallback (callback: CacheClearCallback) {
+        CacheService._clearCallbacks.push(callback);
+    }
+
+    /**
+     * Clear caches
+     */
+    public static async clearCaches () {
+        await reduce(
+            CacheService._clearCallbacks,
+            async (p: Promise<void>, callback: CacheClearCallback) : Promise<void> => {
+                await p;
+                await callback();
+            },
+            Promise.resolve()
+        );
+    }
+
+}
+
+
