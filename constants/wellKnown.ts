542cf90aa4b00b9a93c3481da7d476b1fcbda3cb
Jaakko Heusala <jhh@heusalagroup.fi>
2022-03-24 14:49:18 +0200

    Added health check url as a constant

--- constants/wellKnown.ts
+++ constants/wellKnown.ts
@@ -2,3 +2,4 @@
 
 export const WELL_KNOWN_HG_METADATA_SERVICE_END_POINT = '/.well-known/fi.hg.m.json'
 export const HG_METADATA_SERVICE_GITHUB_ORGANIZATION_NAME = 'fi.hg.github.org';
+export const WELL_KNOWN_HG_HEALTH_CHECK_END_POINT = '/.well-known/fi.hg.up.json';
