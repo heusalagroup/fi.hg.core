9ce2e9811cadaae8ca82db9e400051aaf54ff494
Jaakko Heusala <jhh@heusalagroup.fi>
2022-07-30 16:57:41 +0300

    Moved EmailUtils from frontend library to the core

---
+++ utils/EmailUtils.ts
@@ -0,0 +1,19 @@
+// Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+
+import { isString, trim } from "../modules/lodash";
+
+export class EmailUtils {
+
+    public static isEmailValid (value: string) : boolean {
+        if (!isString(value)) return false;
+        const l = value?.length ?? 0;
+        return l >= 3 && value.includes('@') && !'@.'.includes(value[0]) && !'@.'.includes(value[l-1]);
+    }
+
+    public static getEmailDomain (value: string) : string | undefined {
+        if (!EmailUtils.isEmailValid(value)) return undefined;
+        const atIndex = value.lastIndexOf('@');
+        return atIndex >= 0 ? trim(value.substring(atIndex)) : undefined;
+    }
+
+}
