0819f569e2a19d3131294c71933c9100627faf07
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-10 12:04:35 +0300

    Added type for authentication routes

--- auth/email/constants.ts
+++ auth/email/constants.ts
@@ -3,9 +3,16 @@
 import { Language } from "../../types/Language";
 import { AuthEmailQueryParam } from "./types/AuthEmailQueryParam";
 
-export const AUTHENTICATE_EMAIL_URL = (lang: Language) => `/api/authenticateEmail?${AuthEmailQueryParam.LANGUAGE}=${q(lang)}`;
-export const VERIFY_EMAIL_CODE_URL = (lang: Language) => `/api/verifyEmailCode?${AuthEmailQueryParam.LANGUAGE}=${q(lang)}`;
-export const VERIFY_EMAIL_TOKEN_URL = (lang: Language) => `/api/verifyEmailToken?${AuthEmailQueryParam.LANGUAGE}=${q(lang)}`;
+/**
+ * Callback that uses AuthEmailQueryParam.LANGUAGE as language query parameter
+ */
+export interface CallbackWithLanguage {
+    (lang : Language) : string;
+}
+
+export const AUTHENTICATE_EMAIL_URL : CallbackWithLanguage = (lang: Language) => `/api/authenticateEmail?${AuthEmailQueryParam.LANGUAGE}=${q(lang)}`;
+export const VERIFY_EMAIL_CODE_URL : CallbackWithLanguage = (lang: Language) => `/api/verifyEmailCode?${AuthEmailQueryParam.LANGUAGE}=${q(lang)}`;
+export const VERIFY_EMAIL_TOKEN_URL : CallbackWithLanguage = (lang: Language) => `/api/verifyEmailToken?${AuthEmailQueryParam.LANGUAGE}=${q(lang)}`;
 
 function q (value: string) : string {
     return encodeURIComponent(value);
