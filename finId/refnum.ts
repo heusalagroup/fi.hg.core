f2e9025763b471f18fc2290365dea95ab2043839
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-16 23:41:53 +0300

    Added original legacy finnish ID library

---
+++ finId/refnum.ts
@@ -0,0 +1,154 @@
+/*
+ * Finnish Identity Number Library
+ *
+ * Copyright (C) 2014 by Sendanor <info@sendanor.fi> (http://www.sendanor.fi),
+ * Copyright (C) 2011-2014 by Jaakko-Heikki Heusala <jheusala@iki.fi> (http://www.jhh.me),
+ * Copyright (C) 2009 by Mux F-Production <contact@mux.fi> (http://mux.fi/)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Finnish Referer Number Library (for JavaScript)
+ * by: eXtranium 2009 http://extranium.net/
+ * (c) Sendanor 2009 http://www.sendanor.fi/
+ *
+ * created: Mo 2009-01-05
+ * updated: We 2009-01-07
+ * -----
+ *
+ * Functions:
+ ** refNumLeave_only_digits  - remove non-digits from string
+ ** refNumCalculate_digit    - calculate check-digit needed in referer number
+ ** refNumCreate             - generate referer number
+ ** refNumCheck              - check referer number
+ *
+ */
+
+/** Remove non-digits from string
+ *
+ * Removes any non-digit (0..9) chars from string s
+ *
+ */
+export function refNumLeaveOnlyDigits (s : string | number) : string {
+    let leading = true;
+    s = ''+s;
+    let t = '';		// new string
+    let l = s.length;	// length of old string
+    for (let i=0; i<l; i++) {
+        if(leading) {
+            if(s[i] === '0') {
+                continue;
+            } else if((s[i]>='0') && (s[i]<='9')) {
+                leading = false;
+            }
+        }
+        if ((s[i]>='0') && (s[i]<='9')) t += s[i];	// get only the digits
+    }
+    return t;
+}
+
+/** Calculate check-digit needed in referer number
+ *
+ * function calculates check-digit for complete referer number
+ *
+ *  `num = string of digits ('0'..'9')`
+ *
+ */
+export function refNumCalculateDigit (num : string | number) : string | undefined {
+
+    num = refNumLeaveOnlyDigits(num); // we need only digits 0..9
+
+    let index = num.length;			// set char pointer to end-of-string
+
+    // real referer number is 1..19 + 1 chars long
+    if ( !index || (index>19) ) {
+        return undefined;
+    }
+
+    let calc = 9;	// sum calculator
+    let mult = 0;	// multiplier aka weight (7, 3, 1, 7, 3, 1,...)
+
+    // loop with all digits in num backwards
+    while (index--) {
+        if (!(mult>>=1)) mult = 7;	// update weight (7, 3, 1, 7, 3, 1,...)
+        calc += mult * (num[index].charCodeAt(0)-48);	// add weight*digit to sum
+    } // while
+
+    return String.fromCharCode(57-(calc%10));
+}
+
+/** Generate referer number
+ *
+ * create referer number by adding check-digit to num
+ * `num = string of digits ('0'..'9')`
+ */
+export function refNumCreate (num : string | number) : string {
+    num = refNumLeaveOnlyDigits(num);
+    return num+refNumCalculateDigit(num);
+}
+
+/** Remove check num from reference number
+ * check is referer number legal
+ * `refnum = referer number, string of digits '0'..'9'`
+ */
+export function refNumStrip (refnum : string | number ) : string | undefined {
+
+    refnum = ''+refnum;
+
+    // this becomes to fixed string
+    let t = '';
+
+    for (let i=0; i<refnum.length; i++) {
+        let c = refnum[i];
+        if (c==' ') continue;	// space-bar allowed (but not used in calculations)
+
+        // non-digits not allowed
+        if ((c<'0') || (c>'9')) {
+            return undefined;
+        }
+
+        t += c;
+    } // for
+
+    return t.substring(0,t.length-1);
+}
+
+/** Check referer number */
+export function refNumCheck(refnum : string | number) : boolean {
+    let plain = refNumStrip(refnum);
+    return refNumCreate(plain) === refnum;
+}
+
+/** Parse reference number
+ * @returns {string|undefined} The parsed reference number if valid, otherwise undefined.
+ */
+export function refNumParse(refnum : string | number ) : string | undefined {
+    refnum = refNumLeaveOnlyDigits(refnum);
+    if(refNumCheck(refnum)) {
+        return refnum;
+    }
+}
+
+/** Compare reference numbers
+ * @returns {boolean} True if a equals to b
+ */
+export function refNumCmp (a : string | number, b : string | number) : boolean {
+    a = refNumParse(a);
+    b = refNumParse(b);
+    return a && b && (a === b);
+}
---
+++ finId/ytunnus.ts
@@ -0,0 +1,138 @@
+/*
+ * Finnish Identity Number Library
+ *
+ * Copyright (C) 2022 by Heusala Group <info@hg.fi> (http://www.hg.fi),
+ * Copyright (C) 2014 by Sendanor <info@sendanor.fi> (http://www.sendanor.fi),
+ * Copyright (c) 2014 by Jaakko-Heikki Heusala <jheusala@iki.fi>
+ * Copyright (c) 2014 by Juho Vähäkangas <juhov@iki.fi>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/** Parse integers in base 10
+ * @returns {number} value of `parseInt(n, 10)`
+ */
+function yTunnusParseInt10 (n: any): number {
+    return parseInt(n, 10);
+}
+
+/** Generate Finnish business IDs
+ * @param id_ {string} The business ID with or without checksum
+ * @returns {string} The business ID with checksum
+ */
+export function yTunnusWithSum (id_: string): string {
+    let numbers = [ 7, 9, 10, 5, 8, 4, 2 ];
+    let parts = ('' + id_).split("-");
+    let id = parts.shift();
+    if ( !id ) {
+        throw new TypeError("id not valid: " + id_);
+    }
+    if ( id.length === 6 ) {
+        id = '0' + id;
+    }
+    if ( id.length !== 7 ) {
+        throw new TypeError("id length not valid: " + id_);
+    }
+
+    let id_array = id.split('').map(yTunnusParseInt10);
+
+    if ( id_array.length > 7 ) {
+        throw new TypeError("id array too long (" + id_array.length + ") for " + id_);
+    }
+
+    let sum = id_array.reduce(function (sum, n, i) {
+        let x = numbers[i];
+        if ( typeof n !== "number" ) {
+            throw new TypeError("id[" + i + "] not valid number in " + id_);
+        }
+        return sum + n * x;
+    }, 0);
+
+    sum = (sum % 11);
+    if ( !((sum === 0) || ((sum >= 2) && (sum <= 10))) ) {
+        throw new TypeError("Illegal checksum for " + id_);
+    }
+    sum = (sum === 0) ? 0 : 11 - sum;
+
+    let sum_ = parts.join('-');
+    if ( sum_ && ('' + sum !== sum_) ) {
+        throw new TypeError("Illegal checksum in " + id_);
+    }
+
+    return id + "-" + sum;
+}
+
+/** Check existance of checksum
+ * @param id {string} Finnish business ID
+ * @returns {boolean} `true` if `id` has a checksum
+ */
+function yTunnusHasSum (id: string): boolean {
+    return ('' + id).match("-") !== null;
+}
+
+/** Parse Finnish business ID
+ * @param id {string} ID with or without checksum
+ * @returns {string} ID with checksum
+ */
+export function yTunnusParse (id: string): string {
+    return yTunnusWithSum(id);
+}
+
+/** Non-throwing version of _parse()
+ * @param id {string} The ID to parse
+ * @param defaultValue {string} Optional value to use if errors, defaults to `undefined`.
+ * @returns {string} ID with checksum otherwise `defaultValue`
+ */
+function yTunnusParseNoThrow (id: string, defaultValue ?: string): string {
+    try {
+        return yTunnusParse(id);
+    } catch (err) {
+        return defaultValue;
+    }
+}
+
+/** Compare two business IDs
+ * @param a {string} First business ID
+ * @param b {string} Second business ID
+ * @returns {boolean} `true` if both ids are identical
+ */
+export function yTunnusCompare (a: string, b: string): boolean {
+    return yTunnusParseNoThrow(a) === yTunnusParseNoThrow(b);
+}
+
+/** Check Finnish business ID validity. This function might throw an exception! See _check_nothrow().
+ * @param id {string} The ID to check
+ * @returns {boolean} `true` if ID was valid
+ */
+export function yTunnusCheck (id: string): boolean {
+    id = '' + id;
+    return yTunnusHasSum(id) ? yTunnusCompare(id, yTunnusWithSum(id)) : false;
+}
+
+/** Non-throwing version of _check()
+ * @param id {string} The ID to check
+ * @returns {boolean} `true` if ID was valid
+ */
+function yTunnusCheckNoThrow (id: string): boolean {
+    try {
+        return yTunnusCheck(id);
+    } catch (err) {
+        return false;
+    }
+}
