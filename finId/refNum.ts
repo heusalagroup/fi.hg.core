892c1865c82d6b20b2de785cf36eb17c50701578
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-17 20:19:06 +0300

    Syntax style fixes

--- finId/refNum.ts
+++ finId/refNum.ts
@@ -45,20 +45,20 @@
  * Removes any non-digit (0..9) chars from string s
  *
  */
-export function refNumLeaveOnlyDigits (s : string | number) : string {
+export function refNumLeaveOnlyDigits (s: string | number): string {
     let leading = true;
-    s = ''+s;
+    s = '' + s;
     let t = '';		// new string
     let l = s.length;	// length of old string
-    for (let i=0; i<l; i++) {
-        if(leading) {
-            if(s[i] === '0') {
+    for ( let i = 0 ; i < l ; i++ ) {
+        if ( leading ) {
+            if ( s[i] === '0' ) {
                 continue;
-            } else if((s[i]>='0') && (s[i]<='9')) {
+            } else if ( (s[i] >= '0') && (s[i] <= '9') ) {
                 leading = false;
             }
         }
-        if ((s[i]>='0') && (s[i]<='9')) t += s[i];	// get only the digits
+        if ( (s[i] >= '0') && (s[i] <= '9') ) t += s[i];	// get only the digits
     }
     return t;
 }
@@ -70,14 +70,14 @@ export function refNumLeaveOnlyDigits (s : string | number) : string {
  *  `num = string of digits ('0'..'9')`
  *
  */
-export function refNumCalculateDigit (num : string | number) : string | undefined {
+export function refNumCalculateDigit (num: string | number): string | undefined {
 
     num = refNumLeaveOnlyDigits(num); // we need only digits 0..9
 
     let index = num.length;			// set char pointer to end-of-string
 
     // real referer number is 1..19 + 1 chars long
-    if ( !index || (index>19) ) {
+    if ( !index || (index > 19) ) {
         return undefined;
     }
 
@@ -85,12 +85,12 @@ export function refNumCalculateDigit (num : string | number) : string | undefine
     let mult = 0;	// multiplier aka weight (7, 3, 1, 7, 3, 1,...)
 
     // loop with all digits in num backwards
-    while (index--) {
-        if (!(mult>>=1)) mult = 7;	// update weight (7, 3, 1, 7, 3, 1,...)
-        calc += mult * (num[index].charCodeAt(0)-48);	// add weight*digit to sum
+    while ( index-- ) {
+        if ( !(mult >>= 1) ) mult = 7;	// update weight (7, 3, 1, 7, 3, 1,...)
+        calc += mult * (num[index].charCodeAt(0) - 48);	// add weight*digit to sum
     } // while
 
-    return String.fromCharCode(57-(calc%10));
+    return String.fromCharCode(57 - (calc % 10));
 }
 
 /** Generate referer number
@@ -98,10 +98,10 @@ export function refNumCalculateDigit (num : string | number) : string | undefine
  * create referer number by adding check-digit to num
  * `num = string of digits ('0'..'9')`
  */
-export function refNumCreate (num : string | number) : string | undefined {
+export function refNumCreate (num: string | number): string | undefined {
     const numString = refNumLeaveOnlyDigits(num);
     const digit = refNumCalculateDigit(numString);
-    if (digit === undefined) {
+    if ( digit === undefined ) {
         return undefined;
     }
     return numString + digit;
@@ -111,30 +111,30 @@ export function refNumCreate (num : string | number) : string | undefined {
  * check is referer number legal
  * `refnum = referer number, string of digits '0'..'9'`
  */
-export function refNumStrip (refnum : string | number ) : string | undefined {
+export function refNumStrip (refnum: string | number): string | undefined {
 
-    refnum = ''+refnum;
+    refnum = '' + refnum;
 
     // this becomes to fixed string
     let t = '';
 
-    for (let i=0; i<refnum.length; i++) {
+    for ( let i = 0 ; i < refnum.length ; i++ ) {
         let c = refnum[i];
-        if (c==' ') continue;	// space-bar allowed (but not used in calculations)
+        if ( c == ' ' ) continue;	// space-bar allowed (but not used in calculations)
 
         // non-digits not allowed
-        if ((c<'0') || (c>'9')) {
+        if ( (c < '0') || (c > '9') ) {
             return undefined;
         }
 
         t += c;
     } // for
 
-    return t.substring(0,t.length-1);
+    return t.substring(0, t.length - 1);
 }
 
 /** Check referer number */
-export function refNumCheck(refNum : string | number) : boolean {
+export function refNumCheck (refNum: string | number): boolean {
     const refNumStripped = refNumStrip(refNum);
     const refNumCreated = refNumStripped !== undefined ? refNumCreate(refNumStripped) : undefined;
     return refNumCreated !== undefined && refNumCreated === refNum;
@@ -143,9 +143,9 @@ export function refNumCheck(refNum : string | number) : boolean {
 /** Parse reference number
  * @returns {string|undefined} The parsed reference number if valid, otherwise undefined.
  */
-export function refNumParse(refnum : string | number ) : string | undefined {
+export function refNumParse (refnum: string | number): string | undefined {
     refnum = refNumLeaveOnlyDigits(refnum);
-    if(refNumCheck(refnum)) {
+    if ( refNumCheck(refnum) ) {
         return refnum;
     }
 }
@@ -153,7 +153,7 @@ export function refNumParse(refnum : string | number ) : string | undefined {
 /** Compare reference numbers
  * @returns {boolean} True if a equals to b
  */
-export function refNumCmp (a : string | number, b : string | number) : boolean {
+export function refNumCmp (a: string | number, b: string | number): boolean {
     const a2 = refNumParse(a);
     const b2 = refNumParse(b);
     return !!a2 && !!b2 && (a2 === b2);
