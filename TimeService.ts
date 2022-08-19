a1ee62bd956ca712df00d3380b027d153addf9c7
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-19 21:09:32 +0300

    Improved TimeService

--- TimeService.ts
+++ TimeService.ts
@@ -1,10 +1,19 @@
 // Copyright (c) 2021-2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
+import moment from "moment";
+
 export class TimeService {
 
     public static getCurrentTimeString () : string {
         const now = new Date();
         return now.toISOString();
+    }
+
+    public static getTimeAfterMonths (
+        time: string,
+        months: number
+    ) : string {
+        return moment(time).add(months, "months").toISOString();
     }
 
 }
