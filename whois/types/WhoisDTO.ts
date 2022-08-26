d6a186df73cfe0ef20afbca369c4189ff7e40f39
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-26 23:25:04 +0300

    Modified WhoisDTO

--- whois/types/WhoisDTO.ts
+++ whois/types/WhoisDTO.ts
@@ -1,45 +1,47 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
-import { explain, explainNoOtherKeys, explainRegularObject, hasNoOtherKeys, isRegularObject } from "../../modules/lodash";
+// import { explain, explainNoOtherKeys, explainProperty, explainRegularObject, explainString, hasNoOtherKeys, isRegularObject } from "../../modules/lodash";
+import { WhoisLookupResult } from "../WhoisService";
 
 export interface WhoisDTO {
-    readonly server : string;
+    readonly payload : readonly WhoisLookupResult[];
 }
 
 export function createWhoisDTO (
+    payload: readonly WhoisLookupResult[]
 ) : WhoisDTO {
     return {
-
+        payload
     };
 }
 
-export function isWhoisDTO (value: any) : value is WhoisDTO {
-    return (
-        isRegularObject(value)
-        && hasNoOtherKeys(value, [
-            ''
-        ])
-        //&& isString(value?.foo)
-    );
-}
-
-export function explainWhoisDTO (value: any) : string {
-    return explain(
-        [
-            explainRegularObject(value),
-            explainNoOtherKeys(value, [
-                ''
-            ]),
-            //explainProperty("foo", explainString(value?.foo))
-        ]
-    );
-}
-
-export function stringifyWhoisDTO (value : WhoisDTO) : string {
-    return `WhoisDTO(${value})`;
-}
-
-export function parseWhoisDTO (value: any) : WhoisDTO | undefined {
-    if (isWhoisDTO(value)) return value;
-    return undefined;
-}
+// export function isWhoisDTO (value: any) : value is WhoisDTO {
+//     return (
+//         isRegularObject(value)
+//         && hasNoOtherKeys(value, [
+//             'payload'
+//         ])
+//         && isString(value?.foo)
+//     );
+// }
+//
+// export function explainWhoisDTO (value: any) : string {
+//     return explain(
+//         [
+//             explainRegularObject(value),
+//             explainNoOtherKeys(value, [
+//                 ''
+//             ]),
+//             explainProperty("foo", explainString(value?.foo))
+//         ]
+//     );
+// }
+//
+// export function stringifyWhoisDTO (value : WhoisDTO) : string {
+//     return `WhoisDTO(${value})`;
+// }
+//
+// export function parseWhoisDTO (value: any) : WhoisDTO | undefined {
+//     if (isWhoisDTO(value)) return value;
+//     return undefined;
+// }
