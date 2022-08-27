60970d546e7f597b5f1ed47df9f90ba47fba67e8
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-27 18:55:58 +0300

    Added url encoded content type

--- request/ContentType.ts
+++ request/ContentType.ts
@@ -4,6 +4,7 @@ export enum ContentType {
     TEXT     = "text/plain",
     CALENDAR = "text/calendar",
     JSON     = "application/json",
+    X_WWW_FORM_URLENCODED = "application/x-www-form-urlencoded"
 }
 
 export function isContentType (value: any): value is ContentType {
@@ -11,11 +12,10 @@ export function isContentType (value: any): value is ContentType {
         case ContentType.TEXT:
         case ContentType.CALENDAR:
         case ContentType.JSON:
+        case ContentType.X_WWW_FORM_URLENCODED:
             return true;
-
         default:
             return false;
-
     }
 }
 
@@ -24,12 +24,12 @@ export function stringifyContentType (value: ContentType): string {
         case ContentType.TEXT  : return ContentType.TEXT;
         case ContentType.CALENDAR  : return ContentType.CALENDAR;
         case ContentType.JSON  : return ContentType.JSON;
+        case ContentType.X_WWW_FORM_URLENCODED  : return ContentType.X_WWW_FORM_URLENCODED;
     }
     throw new TypeError(`Unsupported ContentType value: ${value}`);
 }
 
 export function parseContentType (value: any): ContentType | undefined {
-
     switch (`${value}`.toLowerCase()) {
 
         case ContentType.TEXT:
@@ -41,8 +41,9 @@ export function parseContentType (value: any): ContentType | undefined {
         case ContentType.JSON:
         case 'json' : return ContentType.JSON;
 
+        case ContentType.X_WWW_FORM_URLENCODED:
+        case 'X_WWW_FORM_URLENCODED' : return ContentType.X_WWW_FORM_URLENCODED;
+
         default     : return undefined;
-
     }
-
 }
