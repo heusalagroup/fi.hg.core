57972b9fcc93216a932d893fe3b54bc0c4f307c2
Jaakko Heusala <jhh@heusalagroup.fi>
2022-08-27 13:39:12 +0300

    Improved JokerComWhoisService

--- com/joker/JokerComWhoisService.ts
+++ com/joker/JokerComWhoisService.ts
@@ -1,6 +1,13 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
 import { createWhoisServerOptions, WhoisServerOptions } from "../../whois/types/WhoisServerOptions";
+import { WhoisService } from "../../whois/WhoisService";
+import { LogService } from "../../LogService";
+import { WhoisLookupResult } from "../../whois/types/WhoisLookupResult";
+import { first } from "../../modules/lodash";
+import { JokerComWhoisDTO, parseJokerComWhoisDTOFromString } from "./types/JokerComWhoisDTO";
+
+const LOG = LogService.createLogger('JokerComWhoisService');
 
 export const JOKER_WHOIS_HOSTNAME = "whois.joker.com";
 export const JOKER_WHOIS_PORT = 4343;
@@ -12,12 +19,59 @@ export const JOKER_WHOIS_QUERY = "domain:$addr\r\n";
  */
 export class JokerComWhoisService {
 
+    private readonly _whois : WhoisService;
+    private readonly _jokerServer : WhoisServerOptions;
+
+    /**
+     * Returns default joker.com whois server settings
+     */
     public static getJokerServer () : WhoisServerOptions {
         return createWhoisServerOptions(
             JOKER_WHOIS_HOSTNAME,
             JOKER_WHOIS_PORT,
             JOKER_WHOIS_QUERY
         );
+    }
+
+    /**
+     * Create a service for accessing whois.joker.com
+     *
+     * @param service
+     * @param server
+     */
+    constructor (
+        service: WhoisService,
+        server : WhoisServerOptions = JokerComWhoisService.getJokerServer()
+    ) {
+        this._whois = service;
+        this._jokerServer = server;
+    }
+
+    /**
+     * Lookup for a domain name using whois.joker.com
+     *
+     * @param address
+     */
+    public async jokerLookup (
+        address: string
+    ) : Promise<JokerComWhoisDTO> {
+        const server = this._jokerServer;
+        LOG.debug(`lookupJoker: "${address}": server: `, server);
+        const payload : readonly WhoisLookupResult[] = await this._whois.whoisLookup(
+            address,
+            {
+                server,
+                follow: 0
+            }
+        );
+        LOG.debug(`lookupJoker: "${address}": payload: `, payload);
+        const response = first(payload);
+        const dto = response?.data ? parseJokerComWhoisDTOFromString(response?.data) : undefined;
+        if (!dto) {
+            LOG.error(`jokerLookup: Could not parse for "${address}": payload: `, payload);
+            throw new Error(`lookupJoker: Could not parse whois server response for "${address}": ${payload}`);
+        }
+        return dto;
     }
 
 }
