adddddce6d0cf454cfedcdcb6f478b9f6598dbb3
EVCareeria <tuhohami@gmail.com>
2022-11-18 15:09:47 +0200

    Added interface for stream

--- request/MultifileInterface.ts
+++ request/MultifileInterface.ts
@@ -1,13 +1,28 @@
-import ReadableStream = NodeJS.ReadableStream;
 
         // All the MultipartFile fields are methods
 export interface MultipartFileInterface {
    getBytes:() => Blob[];
    getContentType:() => string;
-   getInputStream:() => ReadableStream;
+   getInputStream:() => InputStreamInterface;
    getName:() => string;
    getOriginalFilename:() => string;
    getSize:() => number;
    isEmpty:() => boolean;
    transferTo:() => void;
+}
+
+export interface InputStreamInterface {
+   readable: boolean;
+   setEncoding(encoding?: 'base64' | 'binary'): this;
+   available:() =>number;
+   close:() => boolean;
+   mark:(readLimit?: number) => number;
+   markSupported():boolean;
+   nullInputStream():void;
+   read(mark?:() => number, size?: number): Buffer | number;
+   readAllBytes():void;
+   readNBytes(mark?: () => number, size?: number, toArray?: []):void;
+   reset():this;
+   skip(count: number):void;
+   transferTo(OutputStream?: () => void):void;
 }
