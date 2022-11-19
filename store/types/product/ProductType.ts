f93cdf0b6b3d39157ae6bc1ab120140b903f89b7
Jaakko Heusala <jhh@heusalagroup.fi>
2022-11-19 16:24:47 +0200

    Added more product types

--- store/types/product/ProductType.ts
+++ store/types/product/ProductType.ts
@@ -12,6 +12,7 @@ export enum ProductType {
     SHELL                = "SHELL",
     DATABASE             = "DATABASE",
     WP                   = "WP",
+    NET                  = "NET",
     STOCK                = "STOCK"
 }
 
@@ -26,6 +27,7 @@ export function isProductType (value: any): value is ProductType {
         case ProductType.SHELL:
         case ProductType.DATABASE:
         case ProductType.WP:
+        case ProductType.NET:
         case ProductType.STOCK:
             return true;
         default:
@@ -48,6 +50,7 @@ export function stringifyProductType (value: ProductType): string {
         case ProductType.SHELL : return 'SHELL';
         case ProductType.DATABASE : return 'DATABASE';
         case ProductType.WP : return 'WP';
+        case ProductType.NET : return 'NET';
         case ProductType.STOCK : return 'STOCK';
     }
     throw new TypeError(`Unsupported ProductType value: ${value}`);
@@ -64,6 +67,7 @@ export function parseProductType (value: any): ProductType | undefined {
         case 'SHELL' : return ProductType.SHELL;
         case 'DATABASE' : return ProductType.DATABASE;
         case 'WP' : return ProductType.WP;
+        case 'NET' : return ProductType.NET;
         case 'STOCK' : return ProductType.STOCK;
         default    :
             return undefined;
