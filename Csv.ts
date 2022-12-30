fd7f01917e62af40bc53171efa037d14abd6e848
Jaakko Heusala <jhh@heusalagroup.fi>
2022-12-30 19:47:49 +0200

    Added some documentation

--- Csv.ts
+++ Csv.ts
@@ -1,6 +1,14 @@
 // Copyright (c) 2022. Heusala Group Oy <info@sendanor.fi>. All rights reserved.
 // Copyright (c) 2021. Sendanor <info@sendanor.fi>. All rights reserved.
 
+/**
+ * This module provides functions for working with CSV (Comma Separated Values)
+ * data in TypeScript. It includes functions for parsing CSV data into an array
+ * of rows (each of which is an array of cell values), and for converting an
+ * array of rows into CSV data as a string.
+ * @file
+ */
+
 import {
     endsWith,
     get,
@@ -43,6 +51,14 @@ export interface CsvPropertyTransformerMap<T> {
     [key: string]: CsvPropertyTransformerCallback<T>;
 }
 
+/**
+ * The `stringifyCsvCellValue` function converts a single cell value into a string
+ * suitable for inclusion in a CSV file. It handles arrays by joining the
+ * elements with commas. If the input value is undefined, an empty string is
+ * returned.
+ *
+ * @param value
+ */
 export function stringifyCsvCellValue (value: any) : string {
     if (value === undefined) return '';
     if (isArray(value)) {
@@ -51,10 +67,32 @@ export function stringifyCsvCellValue (value: any) : string {
     return `${value ?? ''}`;
 }
 
+/**
+ * The `getCsvCellFromProperty` function retrieves the value of a property from an
+ * object and converts it to a string suitable for inclusion in a CSV file using
+ * the stringifyCsvCellValue function. It takes an object and the name of the
+ * property to retrieve as parameters.
+ *
+ * @param item
+ * @param key
+ */
 export function getCsvCellFromProperty<T> (item: T, key: string) : string {
     return stringifyCsvCellValue( get(item, key) );
 }
 
+/**
+ * The `getCsvRowFromJsonObject` function generates a row of CSV data from an
+ * object. It takes an object, an array of property names to include in the row,
+ * and an optional map of property transformers as parameters. The property
+ * transformers are functions that take an object and a property name, and
+ * return the value to be included in the row for that property. If no property
+ * transformer is provided for a given property, the value of the property is
+ * retrieved using the `getCsvCellFromProperty` function.
+ *
+ * @param item
+ * @param properties
+ * @param propertyTransformer
+ */
 export function getCsvRowFromJsonObject<T = ReadonlyJsonObject> (
     item: T,
     properties: readonly string[],
@@ -71,6 +109,26 @@ export function getCsvRowFromJsonObject<T = ReadonlyJsonObject> (
     );
 }
 
+/**
+ *
+ * The `getCsvFromJsonObjectList` function generates a CSV file from a list of
+ * objects. It takes a list of objects, an optional array of property names to
+ * include in the file, an optional flag indicating whether to include a header
+ * row with the property names, and an optional map of property transformers as
+ * parameters. It uses the `getCsvRowFromJsonObject` function to generate rows for
+ * each object in the list.
+ *
+ * If no property names are provided, the properties of the first object in the
+ * list are used.
+ *
+ * @param list
+ * @param properties
+ * @param includeHeader If the `includeHeader` flag is `true`, the
+ *                      first row of the CSV file will be a header row with the property names.
+ *                      If the `includeHeader` flag is `false` or not provided, no header row will be
+ *                      included.
+ * @param propertyTransformer
+ */
 export function getCsvFromJsonObjectList<T = ReadonlyJsonObject> (
     list: readonly T[],
     properties: readonly string[] | undefined = undefined,
@@ -106,9 +164,15 @@ export function getCsvFromJsonObjectList<T = ReadonlyJsonObject> (
 }
 
 /**
+ * The `parseCsvRow` function parses a single row of CSV data into an array of
+ * cell values. It takes the row to be parsed as a string, and optional
+ * separator and quote characters as parameters. If no separator character is
+ * provided, the default value of ',' (a comma) is used. If no quote character
+ * is provided, the default value of '"' (a double quote) is used. The function
+ * handles quoted cell values and escaped quote characters within cell values.
+ *
  *
  * @fixme Add support to parse quoted line breaks
- *
  * @param value
  * @param separator
  * @param quote
