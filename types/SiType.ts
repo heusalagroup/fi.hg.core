0428cef579bebf74e23612c44a9c5ccb9c88495a
Jaakko Heusala <jhh@heusalagroup.fi>
2022-12-31 19:33:57 +0200

    Fixed documentation

--- types/SiType.ts
+++ types/SiType.ts
@@ -1,46 +1,67 @@
 // Copyright (c) 2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
 /**
- * @file This module provides utilities for working with SI standard values.
+ * @module
+ * @overview
+ *
+ * This module provides `SiType` enumeration and utilities for working with it.
  *
  * The `SiType` enumeration consists of a set of predefined values representing
  * various units of measurement according to the International System of Units (SI).
+ *
  * The module also provides functions for parsing, stringifying, and checking
  * the validity of these values.
  *
- * @example <caption>To use the SiType enumeration in your code, you will first need to import it
- *          from the module that defines it. You can do this by adding the following line
- *          at the top of your file:</caption>
- *     import { SiType } from "./fi/hg/core/types/SiType";
+ * ### Using the SiType Enumeration
  *
- * @example <caption>You could use the `SiType.KILO` constant in a function like this:</caption>
- *     function convertToKilometers (distanceInMeters: number): number {
- *        return distanceInMeters / 1000;
- *     }
- *     const translations = {
- *         [SiType.KILO]: "kilometers",
- *         // ...add more translations for other SiType values as needed
- *     };
- *     const distanceInKilometers = convertToKilometers(5000);  // 5
- *     console.log(`The distance is ${distanceInKilometers} ${translations[SiType.KILO]}.`);  // The distance is 5 kilometers. *
+ * To use the `SiType` enumeration in your code, you will first need to import it
+ * from the module that defines it.
  *
- * @example <caption>The module provides three utility functions that you can use to work with
- *          SiType values: isSiType, stringifySiType, and parseSiType.</caption>
- *     import { isSiType, stringifySiType, parseSiType } from "./path/to/si-type-module";
+ * You can do this by adding the following line at the top of your file:
  *
- *     // Check if a value is a valid SiType
- *     if (isSiType(SiType.MEGA)) {
- *         console.log("This is a valid SiType value.");
- *     } else {
- *         console.log("This is not a valid SiType value.");
- *     }
+ * ```typescript
+ * import { SiType } from "./fi/hg/core/types/SiType";
+ * ```
  *
- *     // Convert an SiType value to a string
- *     const siTypeString = stringifySiType(SiType.MEGA);  // "MEGA"
+ * Once you have imported the SiType enumeration, you can use the constants it
+ * defines in your code. For example, you could use the SiType.KILO constant in
+ * a function like this:
  *
- *     // Parse a value into an SiType
- *     const siTypeValue = parseSiType("mega");  // SiType.MEGA
+ * ```typescript
+ * function convertToKilometers (distanceInMeters: number): number {
+ *    return distanceInMeters / 1000;
+ * }
+ * const translations = {
+ *     [SiType.KILO]: "kilometers",
+ *     // ...add more translations for other SiType values as needed
+ * };
+ * const distanceInKilometers = convertToKilometers(5000);  // 5
+ * console.log(`The distance is ${distanceInKilometers} ${translations[SiType.KILO]}.`);  // The distance is 5 kilometers. *
+ * ```
  *
+ * ### Using the Utility Functions
+ *
+ * The module provides three utility functions that you can use to work with
+ * `SiType` values: `isSiType`, `stringifySiType`, and `parseSiType`.
+ *
+ * Here's an example of how you might use these functions in your code:
+ *
+ * ```typescript
+ * import { isSiType, stringifySiType, parseSiType } from "./path/to/si-type-module";
+ *
+ * // Check if a value is a valid SiType
+ * if (isSiType(SiType.MEGA)) {
+ *     console.log("This is a valid SiType value.");
+ * } else {
+ *     console.log("This is not a valid SiType value.");
+ * }
+ *
+ * // Convert an SiType value to a string
+ * const siTypeString = stringifySiType(SiType.MEGA);  // "MEGA"
+ *
+ * // Parse a value into an SiType
+ * const siTypeValue = parseSiType("mega");  // SiType.MEGA
+ * ```
  */
 
 import { toUpper } from "lodash";
@@ -65,7 +86,7 @@ export enum SiType {
  * A type guard function that checks if a value is a valid SiType.
  *
  * @param value - The value to check.
- * @returns `true` if the value is a valid SiType, `false` otherwise.
+ * @returns {boolean} `true` if the value is a valid SiType, `false` otherwise.
  * @throws {None} This function does not throw any exceptions.
  * @nosideeffects
  */
@@ -93,7 +114,7 @@ export function isSiType (value: any): value is SiType {
  * Converts an SiType value to its string representation.
  *
  * @param value - The SiType value to stringify.
- * @returns The string representation of the SiType value.
+ * @returns {string} The string representation of the SiType value.
  * @throws {TypeError} If the provided SiType value is not recognized.
  * @nosideeffects
  */
