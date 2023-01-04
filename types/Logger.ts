ee33572c29029d2663650c5a39f8c0dd073cc71e
Jarmo <107168583+Jampsulainen@users.noreply.github.com>
2023-01-04 09:33:57 +0200

    Update Logger.ts

--- types/Logger.ts
+++ types/Logger.ts
@@ -3,17 +3,43 @@
 
 import { LogLevel } from "./LogLevel";
 
+    /**
+     * A logger interface.
+     * @interface
+     */
 export interface Logger {
 
     getLogLevel () : LogLevel | undefined;
+    
+     /**
+     * Set the log level.
+     * @param {LogLevel | undefined} level - The log level to set.
+     * @returns {Logger} - The logger instance.
+     */
     setLogLevel (level : LogLevel | undefined) : Logger;
 
+     /**
+     * Log a debug message.
+     * @param {...any} args - The message arguments.
+     */
     debug (...args: any[]) : void;
 
+     /**
+     * Log an info message.
+     * @param {...any} args - The message arguments.
+     */
     info (...args: any[]) : void;
 
+     /**
+     * Log a warning message.
+     * @param {...any} args - The message arguments.
+     */
     warn (...args: any[]) : void;
 
+     /**
+     * Log an error message.
+     * @param {...any} args - The message arguments.
+     */
     error (...args: any[]) : void;
 
 }
