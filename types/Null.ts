72f861363e14d611dc13a790af7f449574613328
Jaakko Heusala <jhh@hg.fi>
2023-04-09 09:55:52 +0300

    Added explainNull()

--- types/Null.ts
+++ types/Null.ts
@@ -1,1 +1,24 @@
-export {default as isNull} from 'lodash/isNull.js';
+// Copyright (c) 2020-2023. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+
+import { explainNot, explainOk } from "./explain";
+import { default as _isNull } from 'lodash/isNull';
+
+/**
+ *
+ * @param value
+ * @__PURE__
+ * @nosideeffects
+ */
+export function isNull (value: unknown): value is null {
+    return _isNull(value);
+}
+
+/**
+ *
+ * @param value
+ * @__PURE__
+ * @nosideeffects
+ */
+export function explainNull (value: any): string {
+    return isNull(value) ? explainOk() : explainNot('null');
+}
