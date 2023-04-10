2f026f6207f2f9b71bc93d3c32cacabfbca21e7d
Jaakko Heusala <jhh@hg.fi>
2023-04-10 08:17:56 +0300

    Boolean: Added isBooleanOrNullOrUndefined and explainBooleanOrNullOrUndefined

--- types/Boolean.ts
+++ types/Boolean.ts
@@ -3,18 +3,9 @@
 import { isUndefined } from "./undefined";
 import isBoolean from "lodash/isBoolean";
 import { explainOk } from "./explain";
+import { isNull } from "./Null";
 
 export {default as isBoolean} from 'lodash/isBoolean.js';
-
-/**
- *
- * @param value
- * @__PURE__
- * @nosideeffects
- */
-export function isBooleanOrUndefined (value: unknown): value is boolean | undefined {
-    return isUndefined(value) || isBoolean(value);
-}
 
 /**
  *
@@ -32,8 +23,10 @@ export function explainBoolean (value: unknown): string {
  * @__PURE__
  * @nosideeffects
  */
-export function explainBooleanOrUndefined (value: unknown): string {
-    return isBooleanOrUndefined(value) ? explainOk() : 'not boolean or undefined';
+export function parseBoolean (value: any): boolean | undefined {
+    if ( value === undefined || value === '' ) return undefined;
+    if ( isBoolean(value) ) return value;
+    return [ "true", "t", "on", "1", "enabled" ].includes(`${value}`.toLowerCase());
 }
 
 /**
@@ -42,8 +35,38 @@ export function explainBooleanOrUndefined (value: unknown): string {
  * @__PURE__
  * @nosideeffects
  */
-export function parseBoolean (value: any): boolean | undefined {
-    if ( value === undefined || value === '' ) return undefined;
-    if ( isBoolean(value) ) return value;
-    return [ "true", "t", "on", "1", "enabled" ].includes(`${value}`.toLowerCase());
+export function isBooleanOrUndefined (value: unknown): value is boolean | undefined {
+    return isUndefined(value) || isBoolean(value);
 }
+
+/**
+ *
+ * @param value
+ * @__PURE__
+ * @nosideeffects
+ */
+export function explainBooleanOrUndefined (value: unknown): string {
+    return isBooleanOrUndefined(value) ? explainOk() : 'not boolean or undefined';
+}
+
+
+/**
+ *
+ * @param value
+ * @__PURE__
+ * @nosideeffects
+ */
+export function isBooleanOrNullOrUndefined (value: unknown): value is boolean | undefined | null {
+    return isUndefined(value) || isNull(value) || isBoolean(value);
+}
+
+/**
+ *
+ * @param value
+ * @__PURE__
+ * @nosideeffects
+ */
+export function explainBooleanOrNullOrUndefined (value: unknown): string {
+    return isBooleanOrNullOrUndefined(value) ? explainOk() : 'not boolean or undefined or null';
+}
+
