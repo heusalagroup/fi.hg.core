ec17be9ab122d4d891c1a8926ea75445eb27cce4
Jaakko Heusala <jhh@hg.fi>
2023-04-16 12:13:10 +0300

    RequestError: Added support for saving headers in the RequestError exception

--- request/types/RequestError.ts
+++ request/types/RequestError.ts
@@ -1,9 +1,11 @@
+// Copyright (c) 2023 Heusala Group Oy. All rights reserved.
 // Copyright (c) 2020-2021 Sendanor. All rights reserved.
 
 import { RequestStatus, stringifyRequestStatus} from "./RequestStatus";
-import { JsonAny, ReadonlyJsonObject} from "../../Json";
+import { JsonAny, ReadonlyJsonAny, ReadonlyJsonObject } from "../../Json";
 import { RequestType } from "./RequestType";
 import { RequestMethod } from "./RequestMethod";
+import { Headers } from "../Headers";
 
 export class RequestError extends Error {
 
@@ -11,6 +13,7 @@ export class RequestError extends Error {
     public readonly method  : RequestMethod | undefined;
     public readonly url     : string | undefined;
     public readonly body    : JsonAny | undefined;
+    public readonly headers : Headers | undefined;
 
     private readonly __proto__: any;
 
@@ -19,7 +22,8 @@ export class RequestError extends Error {
         message : string        | undefined = undefined,
         method  : RequestMethod | undefined = undefined,
         url     : string        | undefined = undefined,
-        body    : JsonAny       | undefined = undefined
+        body    : JsonAny       | undefined = undefined,
+        headers : Headers       | undefined = undefined
     ) {
 
         super( message ? message : stringifyRequestStatus(status) );
@@ -36,7 +40,7 @@ export class RequestError extends Error {
         this.method  = method;
         this.url     = url;
         this.body    = body;
-
+        this.headers = headers;
     }
 
     public valueOf () : number {
@@ -51,7 +55,11 @@ export class RequestError extends Error {
         return {
             type: RequestType.ERROR,
             status: this.status,
-            message: this.message
+            message: this.message,
+            method: this.method,
+            url: this.url,
+            body: (this.body as ReadonlyJsonAny | undefined),
+            headers: this.headers ? this.headers.toJSON() : undefined,
         };
     }
 
@@ -69,6 +77,10 @@ export class RequestError extends Error {
 
     public getBody () : JsonAny | undefined {
         return this.body;
+    }
+
+    public getHeaders () : Headers | undefined {
+        return this.headers;
     }
 
 }
