b13a8047e68136281c0b80aa2db9745e789ad6c1
Jaakko Heusala <jhh@hg.fi>
2023-04-23 14:44:05 +0300

    Added a type definition for ISO date strings

---
+++ types/IsoDateString.ts
@@ -0,0 +1,36 @@
+// Copyright (c) 2023. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+
+import { isString } from "./String";
+import { explainNot, explainOk } from "./explain";
+import { createJestPassObject } from "../../jest/types/JestPassObject";
+
+export type IsoDateString = string;
+
+export function createIsoDateString (
+    value: string
+) : IsoDateString {
+    const parsedValue = parseIsoDateString(value);
+    if (!parsedValue) throw new TypeError(`Value is not ISO data string: '${value}'`);
+    return parsedValue;
+}
+
+export function isIsoDateString (value: unknown) : value is IsoDateString {
+    if(!isString(value)) return false;
+    if ( !/\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/.test(value) ) return false;
+    const d = new Date(value);
+    return d.toISOString() === value;
+}
+
+export function explainIsoDateString (value: unknown) : string {
+    return isIsoDateString(value) ? explainOk() : explainNot(`Expected '${value}' to be a valid IsoDateString`);
+}
+
+export function stringifyIsoDateString (value : IsoDateString) : string {
+    return `IsoDateString(${value})`;
+}
+
+export function parseIsoDateString (value: unknown) : IsoDateString | undefined {
+    if (value instanceof Date) value = value.toISOString();
+    if (isIsoDateString(value)) return value;
+    return undefined;
+}
