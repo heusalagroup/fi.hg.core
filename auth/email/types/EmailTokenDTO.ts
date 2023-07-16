925d0ec242d2d8e7b219ece5228b9810106ee2b5
Jaakko Heusala <jhh@hg.fi>
2023-07-16 18:27:51 +0300

    Added helper functions for EmailTokenDTO

--- auth/email/types/EmailTokenDTO.ts
+++ auth/email/types/EmailTokenDTO.ts
@@ -5,11 +5,25 @@ import { isBooleanOrUndefined } from "../../../types/Boolean";
 import { isString } from "../../../types/String";
 import { isRegularObject } from "../../../types/RegularObject";
 import { hasNoOtherKeysInDevelopment } from "../../../types/OtherKeys";
+import { isUndefined } from "../../../types/undefined";
+import { explainNot, explainOk, explainOr } from "../../../types/explain";
 
 export interface EmailTokenDTO {
     readonly token     : string;
     readonly email     : string;
     readonly verified ?: boolean | undefined;
+}
+
+export function createEmailTokenDTO (
+    token     : string,
+    email     : string,
+    verified ?: boolean | undefined,
+) {
+    return {
+        token,
+        email,
+        verified,
+    };
 }
 
 export function isEmailTokenDTO (value: any): value is EmailTokenDTO {
@@ -33,4 +47,12 @@ export function stringifyEmailTokenDTO (value: EmailTokenDTO): string {
 export function parseEmailTokenDTO (value: any): EmailTokenDTO | undefined {
     if ( isEmailTokenDTO(value) ) return value;
     return undefined;
+}
+
+export function isEmailTokenDTOOrUndefined (value: unknown): value is EmailTokenDTO | undefined {
+    return isUndefined(value) || isEmailTokenDTO(value);
+}
+
+export function explainEmailTokenDTOOrUndefined (value: unknown): string {
+    return isEmailTokenDTOOrUndefined(value) ? explainOk() : explainNot(explainOr(['EmailTokenDTO', 'undefined']));
 }
