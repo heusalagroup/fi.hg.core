4196ad2a6d875156f68df94d53d201d8122efa2a
Jaakko Heusala <jhh@hg.fi>
2023-07-21 01:24:04 +0300

    Query escaped url parameters

--- op/op-constants.ts
+++ op/op-constants.ts
@@ -4,5 +4,9 @@ export const OP_PRODUCTION_URL = 'https://corporate-api.apiauth.services.op.fi';
 export const OP_SANDBOX_URL = 'https://sandbox-api.apiauth.aws.op-palvelut.net';
 
 export const OP_ACCOUNT_DATA_GET_ACCOUNT_LIST_PATH = `/corporate-account-data/v1/accounts`
-export const OP_ACCOUNT_DATA_GET_ACCOUNT_DETAILS_PATH = (surrogateId: string) => `/corporate-account-data/v1/accounts/${surrogateId}`
-export const OP_ACCOUNT_DATA_GET_TRANSACTION_LIST_PATH = (surrogateId: string) => `/corporate-account-data/v2/accounts/${surrogateId}/transactions`
+export const OP_ACCOUNT_DATA_GET_ACCOUNT_DETAILS_PATH = (surrogateId: string) => `/corporate-account-data/v1/accounts/${q(surrogateId)}`
+export const OP_ACCOUNT_DATA_GET_TRANSACTION_LIST_PATH = (surrogateId: string) => `/corporate-account-data/v2/accounts/${q(surrogateId)}/transactions`
+
+function q (value: string) : string {
+    return encodeURIComponent(value);
+}
