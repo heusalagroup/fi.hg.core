53b7902be66817024b931cf690026ae649c9a3cc
Jaakko Heusala <jhh@hg.fi>
2023-08-03 16:37:04 +0300

    Updated translations

--- auth/sms/sms-auth-translations.ts
+++ auth/sms/sms-auth-translations.ts
@@ -6,23 +6,23 @@ export const DEFAULT_LANGUAGE : Language = Language.ENGLISH;
 
 // Email message translation codes (used at backend code)
 
-export const T_M_COMMON_SITE_NAME    = 'm.common.siteName';
-export const T_M_COMMON_COMPANY_NAME = 'm.common.companyName';
+export const T_M_COMMON_SITE_NAME    = 'sms.m.common.siteName';
+export const T_M_COMMON_COMPANY_NAME = 'sms.m.common.companyName';
 
-export const T_M_COMMON_HEADER_TEXT = 'm.common.headerText';
-export const T_M_COMMON_FOOTER_TEXT = 'm.common.footerText';
+export const T_M_COMMON_HEADER_TEXT = 'sms.m.common.headerText';
+export const T_M_COMMON_FOOTER_TEXT = 'sms.m.common.footerText';
 
-export const T_M_COMMON_HEADER_HTML = 'm.common.headerHtml';
-export const T_M_COMMON_FOOTER_HTML = 'm.common.footerHtml';
+export const T_M_COMMON_HEADER_HTML = 'sms.m.common.headerHtml';
+export const T_M_COMMON_FOOTER_HTML = 'sms.m.common.footerHtml';
 
 // Auth code email
 
-export const T_M_AUTH_CODE_SUBJECT   = 'm.authCode.subject';
+export const T_M_AUTH_CODE_SUBJECT   = 'sms.m.authCode.subject';
 
-export const T_M_AUTH_CODE_HEADER_TEXT = 'm.authCode.headerText';
-export const T_M_AUTH_CODE_BODY_TEXT   = 'm.authCode.bodyText';
-export const T_M_AUTH_CODE_FOOTER_TEXT = 'm.authCode.footerText';
+export const T_M_AUTH_CODE_HEADER_TEXT = 'sms.m.authCode.headerText';
+export const T_M_AUTH_CODE_BODY_TEXT   = 'sms.m.authCode.bodyText';
+export const T_M_AUTH_CODE_FOOTER_TEXT = 'sms.m.authCode.footerText';
 
-export const T_M_AUTH_CODE_HEADER_HTML = 'm.authCode.headerHtml';
-export const T_M_AUTH_CODE_BODY_HTML   = 'm.authCode.bodyHtml';
-export const T_M_AUTH_CODE_FOOTER_HTML = 'm.authCode.footerHtml';
+export const T_M_AUTH_CODE_HEADER_HTML = 'sms.m.authCode.headerHtml';
+export const T_M_AUTH_CODE_BODY_HTML   = 'sms.m.authCode.bodyHtml';
+export const T_M_AUTH_CODE_FOOTER_HTML = 'sms.m.authCode.footerHtml';
