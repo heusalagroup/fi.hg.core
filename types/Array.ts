b3f0694c5c3861bfbccf8839e38780186ed40fee
Jaakko Heusala <jhh@hg.fi>
2023-12-06 02:34:58 +0200

    Added test functions for pair, triplet and tetrad arrays

--- types/Array.ts
+++ types/Array.ts
@@ -122,6 +122,174 @@ export function explainArrayOf<T = any> (
 /**
  *
  * @param value
+ * @param isItemA
+ * @param isItemB
+ * @__PURE__
+ * @nosideeffects
+ */
+export function isPairArrayOf<T1 = any, T2 = T1> (
+    value: unknown,
+    isItemA: TestCallback,
+    isItemB: TestCallback = isItemA,
+): value is [T1, T2] | readonly [T1, T2] {
+    if ( !_isArray(value) ) return false;
+    const len : number = value?.length ?? 0;
+    if ( len !== 2 ) {
+        return false;
+    }
+    const [a, b] = value;
+    return isItemA(a, 0, value) && isItemB(b, 1, value);
+}
+
+/**
+ *
+ * @param value
+ * @param isItemA
+ * @param isItemB
+ * @param itemTypeNameA
+ * @param itemTypeNameB
+ * @__PURE__
+ * @nosideeffects
+ */
+export function explainPairArrayOf<T1 = any, T2 = T1> (
+    value: unknown,
+    itemTypeNameA: string,
+    isItemA: TestCallback,
+    itemTypeNameB: string = itemTypeNameA,
+    isItemB: TestCallback = isItemA,
+): string {
+    return isPairArrayOf<T1, T2>( value, isItemA, isItemB ) ? explainOk() : explainNot( `[${itemTypeNameA}, ${itemTypeNameB}]` );
+}
+
+/**
+ *
+ * @param value
+ * @param isItemA
+ * @param isItemB
+ * @param isItemC
+ * @__PURE__
+ * @nosideeffects
+ */
+export function isTripletArrayOf<T1, T2, T3> (
+    value: unknown,
+    isItemA: TestCallback,
+    isItemB: TestCallback,
+    isItemC: TestCallback,
+): value is [T1, T2, T3] | readonly [T1, T2, T3] {
+    if ( !_isArray(value) ) return false;
+    const len : number = value?.length ?? 0;
+    if ( len !== 3 ) {
+        return false;
+    }
+    const [a, b, c] = value;
+    return (
+        isItemA(a, 0, value)
+        && isItemB(b, 1, value)
+        && isItemC(c, 2, value)
+    );
+}
+
+/**
+ *
+ * @param value
+ * @param isItemA
+ * @param isItemB
+ * @param isItemC
+ * @param itemTypeNameA
+ * @param itemTypeNameB
+ * @param itemTypeNameC
+ * @__PURE__
+ * @nosideeffects
+ */
+export function explainTripletArrayOf<T1, T2, T3> (
+    value: unknown,
+    itemTypeNameA: string,
+    isItemA: TestCallback,
+    itemTypeNameB: string,
+    isItemB: TestCallback,
+    itemTypeNameC: string,
+    isItemC: TestCallback,
+): string {
+    return isTripletArrayOf<T1, T2, T3>(
+        value,
+        isItemA,
+        isItemB,
+        isItemC,
+    ) ? explainOk() : explainNot(
+        `[${itemTypeNameA}, ${itemTypeNameB}, ${itemTypeNameC}]`
+    );
+}
+
+/**
+ *
+ * @param value
+ * @param isItemA
+ * @param isItemB
+ * @param isItemC
+ * @param isItemD
+ * @__PURE__
+ * @nosideeffects
+ */
+export function isTetradArrayOf<T1, T2, T3, T4> (
+    value: unknown,
+    isItemA: TestCallback,
+    isItemB: TestCallback,
+    isItemC: TestCallback,
+    isItemD: TestCallback,
+): value is [T1, T2, T3, T4] | readonly [T1, T2, T3, T4] {
+    if ( !_isArray(value) ) return false;
+    const len : number = value?.length ?? 0;
+    if ( len !== 4 ) {
+        return false;
+    }
+    const [a, b, c, d] = value;
+    return (
+        isItemA(a, 0, value)
+        && isItemB(b, 1, value)
+        && isItemC(c, 2, value)
+        && isItemD(d, 3, value)
+    );
+}
+
+/**
+ *
+ * @param value
+ * @param isItemA
+ * @param isItemB
+ * @param isItemC
+ * @param isItemD
+ * @param itemTypeNameA
+ * @param itemTypeNameB
+ * @param itemTypeNameC
+ * @param itemTypeNameD
+ * @__PURE__
+ * @nosideeffects
+ */
+export function explainTetradArrayOf<T1, T2, T3, T4> (
+    value: unknown,
+    itemTypeNameA: string,
+    isItemA: TestCallback,
+    itemTypeNameB: string,
+    isItemB: TestCallback,
+    itemTypeNameC: string,
+    isItemC: TestCallback,
+    itemTypeNameD: string,
+    isItemD: TestCallback,
+): string {
+    return isTetradArrayOf<T1, T2, T3, T4>(
+        value,
+        isItemA,
+        isItemB,
+        isItemC,
+        isItemD,
+    ) ? explainOk() : explainNot(
+        `[${itemTypeNameA}, ${itemTypeNameB}, ${itemTypeNameC}, ${itemTypeNameD}]`
+    );
+}
+
+/**
+ *
+ * @param value
  * @param isItem
  * @param minLength
  * @param maxLength
